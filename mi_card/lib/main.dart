574dc45671efa18f74529d4f936fd517624af406
Kelvin <kelvinw@iastate.edu>
2019-06-03 21:42:01 -0500

    added Column and Containers for basic layout

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -8,7 +8,37 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea( 
+          child: Column ( 
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                child: Text(" 'tainer One "),
+                color: Colors.greenAccent,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.greenAccent,
+                child: Text(" 'tainer Two "),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.greenAccent,
+                child: Text(" 'tainer Three "),
+              ),
+             ],
+          ),
+        ),
+      )
+    );
   }
 }
-
